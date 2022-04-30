21f4bb856bee4c47cb6517ed8dba5b8b3accc794
bree parker <breannaparker1991@gmail.com>
2022-04-30 17:37:01 -0600

    first_api_complete

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 // GET /users/breannaparker1991
 // curl -i -u breannaparker1991:$token https://api.github.com/users/breannaparker1991
 
-fetch("https://api.github.com/users/breannaparker1991")
-  .then(response => response.json() )
-  .then(coderData => console.log(coderData) )
-  .catch(err => console.log(err) )
+// fetch("https://api.github.com/users/breannaparker1991")
+//   .then(response => response.json() )
+//   .then(coderData => console.log(coderData) )
+//   .catch(err => console.log(err) )
 
 
 async function getCoderData() {
   var response = await fetch("https://api.github.com/users/breannaparker1991");
   var coderData = await response.json();
-  return coderData;
+  var imgprofile = document.createElement('img')
+  imgprofile.src = coderData.avatar_url
+  var imgdiv = document.getElementById('profilepic')
+  imgdiv.appendChild(imgprofile)
+  document.getElementById('followers').innerHTML = coderData.followers
+  console.log(coderData);
 }
         
-console.log(getCoderData());
+// console.log(getCoderData());
     
